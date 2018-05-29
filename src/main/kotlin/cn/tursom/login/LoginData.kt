1d958cf28aa99c85d4ad1e59b8762bebfcaa181e
Tursom Ulefits <tursom@foxmail.com>
2018-05-29 22:31:12 +0800

    重构项目

--- src/main/kotlin/cn/tursom/login/LoginData.kt
+++
@@ -1,4 +0,0 @@
-package cn.tursom.login
-
-data class LoginData(val id: Int?, val name: String?, val time: String?, val password: String?)
-
--- src/main/kotlin/cn/tursom/register/RegisterData.kt
+++
@@ -1,3 +0,0 @@
-package cn.tursom.register
-
-data class RegisterData(val id: Int?, val name: String?, val time: String?, val password: String?)
