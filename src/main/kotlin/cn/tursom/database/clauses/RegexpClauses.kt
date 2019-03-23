a5c2d8256658573f90d14c6c205219dbffd26f5d
tursom <tursom@foxmail.com>
2019-03-23 12:54:01 +0800

    为RegexpClauses添加了几个构造函数

--- src/main/kotlin/cn/tursom/database/clauses/RegexpClauses.kt
+++ src/main/kotlin/cn/tursom/database/clauses/RegexpClauses.kt
@@ -3,6 +3,7 @@ package cn.tursom.database.clauses
 import cn.tursom.database.fieldName
 import cn.tursom.database.sqlStr
 import cn.tursom.regex.RegexMaker
+import cn.tursom.regex.RegexUnit
 import java.lang.reflect.Field
 import kotlin.reflect.KProperty
 import kotlin.reflect.jvm.javaField
@@ -12,13 +13,21 @@ class RegexpClauses(val field: String, val value: String) : Clause {
 	constructor(field: Field, value: String) : this(field.fieldName, value)
 	constructor(field: KProperty<*>, value: String) : this(field.javaField!!, value)
 	
-	constructor(field: String, value: RegexMaker.() -> String)
+	constructor(field: String, value: Regex) : this(field, value.toString())
+	constructor(field: Field, value: Regex) : this(field, value.toString())
+	constructor(field: KProperty<*>, value: Regex) : this(field, value.toString())
+	
+	constructor(field: String, value: RegexUnit) : this(field, value.toString())
+	constructor(field: Field, value: RegexUnit) : this(field, value.toString())
+	constructor(field: KProperty<*>, value: RegexUnit) : this(field, value.toString())
+	
+	constructor(field: String, value: RegexMaker.() -> RegexUnit)
 		: this(field, RegexMaker.value())
 	
-	constructor(field: Field, value: RegexMaker.() -> String)
+	constructor(field: Field, value: RegexMaker.() -> RegexUnit)
 		: this(field, RegexMaker.value())
 	
-	constructor(field: KProperty<*>, value: RegexMaker.() -> String)
+	constructor(field: KProperty<*>, value: RegexMaker.() -> RegexUnit)
 		: this(field, RegexMaker.value())
 	
 	override val sqlStr: String
