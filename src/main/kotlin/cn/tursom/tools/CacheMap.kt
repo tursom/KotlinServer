064879b98b0c5997a5b80a96b7122f070a1c893a
tursom <tursom@foxmail.com>
2019-05-28 13:51:01 +0800

    添加了协程安全的缓存类

--- src/main/kotlin/cn/tursom/tools/CacheMap.kt
+++ src/main/kotlin/cn/tursom/tools/CacheMap.kt
@@ -1,5 +1,47 @@
 package cn.tursom.tools
 
-class CacheMap<K, V> {
+import cn.tursom.asynclock.ReadWriteLockHashMap
 
+class CacheMap<K, V>(val timeout: Long) {
+	private val valueMap = ReadWriteLockHashMap<K, Pair<Long, V>>()
+	
+	suspend fun get(key: K): V? {
+		val (time, value) = getCache(key) ?: return null
+		if (time.isTimeOut()) {
+			delCache(key)
+			return null
+		}
+		return value
+	}
+	
+	suspend fun get(key: K, constroutor: suspend () -> V): V {
+		val (time, value) = getCache(key) ?: run {
+			val newValue = System.currentTimeMillis() to constroutor()
+			addCache(key, newValue)
+			newValue
+		}
+		if (time.isTimeOut()) {
+			delCache(key)
+			return run {
+				val newValue = constroutor()
+				addCache(key, System.currentTimeMillis() to newValue)
+				newValue
+			}
+		}
+		return value
+	}
+	
+	private suspend fun getCache(key: K): Pair<Long, V>? {
+		return valueMap.get(key)
+	}
+	
+	private suspend fun delCache(key: K) {
+		valueMap.remove(key)
+	}
+	
+	private suspend fun addCache(key: K, value: Pair<Long, V>) {
+		valueMap.set(key, value)
+	}
+	
+	private fun Long.isTimeOut() = timeout != 0L && System.currentTimeMillis() - this > timeout
 }
