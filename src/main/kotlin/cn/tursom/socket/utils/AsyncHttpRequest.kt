0b4e23310e266176a3698d1228408d9f4168bbc7
tursom <tursom@foxmail.com>
2019-05-28 17:46:35 +0800

    添加了不需要OkHttp也能调用的接口，同时增加了获取ByteArray的接口

--- src/main/kotlin/cn/tursom/socket/utils/AsyncHttpRequest.kt
+++ src/main/kotlin/cn/tursom/socket/utils/AsyncHttpRequest.kt
@@ -14,9 +14,12 @@ import kotlin.coroutines.suspendCoroutine
 @Suppress("unused", "MemberVisibilityCanBePrivate")
 object AsyncHttpRequest {
 	
-	val defaultClient = OkHttpClient()
+	val defaultClient: OkHttpClient = OkHttpClient().newBuilder()
+		.retryOnConnectionFailure(true)
+		.build()
 	val socketClient = proxyClient()
 	val httpProxyClient = proxyClient(port = 8080, type = Proxy.Type.HTTP)
+	
 	
 	fun proxyClient(
 		host: String = "127.0.0.1",
@@ -24,6 +27,7 @@ object AsyncHttpRequest {
 		type: Proxy.Type = Proxy.Type.SOCKS
 	): OkHttpClient = OkHttpClient().newBuilder()
 		.proxy(Proxy(type, InetSocketAddress(host, port)))
+		.retryOnConnectionFailure(true)
 		.build()
 	
 	private suspend fun sendRequest(call: Call): Response = suspendCoroutine {
@@ -34,22 +38,6 @@ object AsyncHttpRequest {
 			
 			override fun onResponse(call: Call, response: Response) {
 				it.resume(response)
-			}
-		})
-	}
-	
-	private suspend fun requestString(call: Call): String = suspendCoroutine {
-		call.enqueue(object : Callback {
-			override fun onFailure(call: Call, e: IOException) {
-				it.resumeWithException(e)
-			}
-			
-			override fun onResponse(call: Call, response: Response) {
-				try {
-					it.resume(response.body()!!.string())
-				} catch (e: Throwable) {
-					it.resumeWithException(e)
-				}
 			}
 		})
 	}
@@ -146,48 +134,36 @@ object AsyncHttpRequest {
 	suspend fun getStr(
 		url: String,
 		param: Map<String, String>? = null,
-		headers: Map<String, String>? = null,
-		client: OkHttpClient = defaultClient
-	): String {
-		val paramSB = StringBuilder()
-		param?.forEach {
-			paramSB.append("${URLEncoder.encode(it.key, "UTF-8")}=${URLEncoder.encode(it.value, "UTF-8")}&")
-		}
-		if (paramSB.isNotEmpty())
-			paramSB.deleteCharAt(paramSB.length - 1)
-		
-		val requestBuilder = Request.Builder().get()
-			.url("$url?$paramSB")
-		
-		headers?.forEach { t, u ->
-			requestBuilder.addHeader(t, u)
-		}
-		
-		return requestString(client.newCall(requestBuilder.build()))
-	}
+		headers: Map<String, String>? = null
+	): String = getStr(url, param, headers, defaultClient)
 	
+	@Suppress("BlockingMethodInNonBlockingContext")
+	suspend fun getStr(
+		url: String,
+		param: Map<String, String>? = null,
+		headers: Map<String, String>? = null,
+		client: OkHttpClient
+	): String = get(url, param, headers, client).body()!!.string()
+	
+	@Suppress("BlockingMethodInNonBlockingContext")
 	private suspend fun postStr(
 		url: String,
 		body: RequestBody,
 		headers: Map<String, String>? = null,
-		client: OkHttpClient = defaultClient
-	): String {
-		val requestBuilder = Request.Builder()
-			.post(body)
-			.url(url)
-		
-		headers?.forEach { t, u ->
-			requestBuilder.addHeader(t, u)
-		}
-		
-		return requestString(client.newCall(requestBuilder.build()))
-	}
+		client: OkHttpClient
+	): String = post(url, body, headers, client).body()!!.string()
+	
+	suspend fun postStr(
+		url: String,
+		param: Map<String, String>,
+		headers: Map<String, String>? = null
+	): String = postStr(url, param, headers, defaultClient)
 	
 	suspend fun postStr(
 		url: String,
 		param: Map<String, String>,
 		headers: Map<String, String>? = null,
-		client: OkHttpClient = defaultClient
+		client: OkHttpClient
 	): String {
 		val formBuilder = FormBody.Builder()
 		param.forEach { (t, u) ->
@@ -199,8 +175,14 @@ object AsyncHttpRequest {
 	suspend fun postStr(
 		url: String,
 		body: String,
+		headers: Map<String, String>? = null
+	): String = postStr(url, body, headers, defaultClient)
+	
+	suspend fun postStr(
+		url: String,
+		body: String,
 		headers: Map<String, String>? = null,
-		client: OkHttpClient = defaultClient
+		client: OkHttpClient
 	): String = postStr(
 		url,
 		RequestBody.create(MediaType.parse("text/plain;charset=utf-8"), body),
@@ -211,8 +193,14 @@ object AsyncHttpRequest {
 	suspend fun postStr(
 		url: String,
 		body: File,
+		headers: Map<String, String>? = null
+	): String = postStr(url, body, headers, defaultClient)
+	
+	suspend fun postStr(
+		url: String,
+		body: File,
 		headers: Map<String, String>? = null,
-		client: OkHttpClient = defaultClient
+		client: OkHttpClient
 	): String = postStr(
 		url,
 		RequestBody.create(MediaType.parse("application/octet-stream"), body),
@@ -220,4 +208,82 @@ object AsyncHttpRequest {
 		client
 	)
 	
+	suspend fun getByteArray(
+		url: String,
+		param: Map<String, String>? = null,
+		headers: Map<String, String>? = null
+	): ByteArray = getByteArray(url, param, headers, defaultClient)
+	
+	@Suppress("BlockingMethodInNonBlockingContext")
+	suspend fun getByteArray(
+		url: String,
+		param: Map<String, String>? = null,
+		headers: Map<String, String>? = null,
+		client: OkHttpClient
+	): ByteArray = get(url, param, headers, client).body()!!.bytes()
+	
+	
+	@Suppress("BlockingMethodInNonBlockingContext")
+	private suspend fun postByteArray(
+		url: String,
+		body: RequestBody,
+		headers: Map<String, String>? = null,
+		client: OkHttpClient
+	): ByteArray = post(url, body, headers, client).body()!!.bytes()
+	
+	
+	suspend fun postByteArray(
+		url: String,
+		param: Map<String, String>,
+		headers: Map<String, String>? = null
+	): ByteArray = postByteArray(url, param, headers, defaultClient)
+	
+	suspend fun postByteArray(
+		url: String,
+		param: Map<String, String>,
+		headers: Map<String, String>? = null,
+		client: OkHttpClient
+	): ByteArray {
+		val formBuilder = FormBody.Builder()
+		param.forEach { (t, u) ->
+			formBuilder.add(t, u)
+		}
+		return postByteArray(url, formBuilder.build(), headers, client)
+	}
+	
+	suspend fun postByteArray(
+		url: String,
+		body: String,
+		headers: Map<String, String>? = null
+	): ByteArray = postByteArray(url, body, headers, defaultClient)
+	
+	suspend fun postByteArray(
+		url: String,
+		body: String,
+		headers: Map<String, String>? = null,
+		client: OkHttpClient
+	): ByteArray = postByteArray(
+		url,
+		RequestBody.create(MediaType.parse("text/plain;charset=utf-8"), body),
+		headers,
+		client
+	)
+	
+	suspend fun postByteArray(
+		url: String,
+		body: File,
+		headers: Map<String, String>? = null
+	): ByteArray = postByteArray(url, body, headers, defaultClient)
+	
+	suspend fun postByteArray(
+		url: String,
+		body: File,
+		headers: Map<String, String>? = null,
+		client: OkHttpClient
+	): ByteArray = postByteArray(
+		url,
+		RequestBody.create(MediaType.parse("application/octet-stream"), body),
+		headers,
+		client
+	)
 }
