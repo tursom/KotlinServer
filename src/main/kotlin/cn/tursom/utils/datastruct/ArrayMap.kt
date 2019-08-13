326d642945428454df53338044615fd663e71930
tursom <tursom@foxmail.com>
2019-08-13 18:49:30 +0800

    修复了ArrayMap的BUGs

--- src/main/kotlin/cn/tursom/utils/datastruct/ArrayMap.kt
+++ src/main/kotlin/cn/tursom/utils/datastruct/ArrayMap.kt
@@ -5,28 +5,29 @@ class ArrayMap<K : Comparable<K>, V> : Map<K, V> {
 	private var arr: Array<Node<K, V>?> = Array(4) { null }
 	@Volatile
 	private var end = 0
-	
+
 	override val size: Int
 		get() = end
 	override val entries: Set<Map.Entry<K, V>> = EntrySet(this)
 	override val keys: Set<K> = KeySet(this)
 	override val values: Collection<V> = ValueCollection(this)
-	
+
 	fun clear() {
 		end = 0
 	}
-	
+
 	operator fun set(key: K, value: V): V? {
 		val index = search(key)
-		if (index < 0) insert(key, value, -index - 1)
-		else {
+		if (index < 0) {
+			insert(key, value, -index - 1)
+		} else {
 			val node = arr[index]
 			if (node != null) node.value = value
 			else arr[index] = Node(key, value)
 		}
 		return value
 	}
-	
+
 	infix fun putAll(from: Map<out K, V>) {
 		from.forEach { (key, value) ->
 			val index = search(key)
@@ -39,7 +40,7 @@ class ArrayMap<K : Comparable<K>, V> : Map<K, V> {
 		}
 		arr.sort()
 	}
-	
+
 	infix fun remove(key: K): V? {
 		val index = search(key)
 		if (index >= 0) {
@@ -51,28 +52,28 @@ class ArrayMap<K : Comparable<K>, V> : Map<K, V> {
 		}
 		return null
 	}
-	
+
 	override infix fun containsKey(key: K): Boolean {
 		return search(key) >= 0
 	}
-	
+
 	override infix fun containsValue(value: V): Boolean {
 		arr.forEach {
 			if (it?.value == value) return true
 		}
 		return false
 	}
-	
+
 	override infix operator fun get(key: K): V? {
 		val index = search(key)
 		return if (index < 0) null
 		else arr[index]?.value
 	}
-	
+
 	override fun isEmpty(): Boolean {
 		return end == 0
 	}
-	
+
 	override fun toString(): String {
 		if (end == 0) return "{}"
 		val sb = StringBuilder("{${arr[0]}")
@@ -83,7 +84,7 @@ class ArrayMap<K : Comparable<K>, V> : Map<K, V> {
 		sb.append("}")
 		return sb.toString()
 	}
-	
+
 	private fun resize() {
 		val oldArr = arr
 		arr = Array(arr.size * 2) { null }
@@ -91,139 +92,138 @@ class ArrayMap<K : Comparable<K>, V> : Map<K, V> {
 			arr[i] = v
 		}
 	}
-	
+
 	private fun insert(key: K, value: V, index: Int): V? {
 		if (end == arr.size) resize()
-		for (i in end++ until index) {
-			arr[i] = arr[i - 1]
-		}
+		for (i in end - 2 downTo index) arr[i + 1] = arr[i]
 		arr[index] = Node(key, value)
+		end++
 		return value
 	}
-	
+
 	private fun search(key: K): Int {
 		if (end == 0) return -1
-		return arr.binarySearch(key, 0, end)
+		return arr.binarySearch(key, 0, end - 1)
 	}
-	
+
 	class Node<K : Comparable<K>, V>(
-		override val key: K,
-		@Volatile override var value: V
+			override val key: K,
+			@Volatile override var value: V
 	) : Comparable<K>, Map.Entry<K, V> {
 		override fun compareTo(other: K): Int {
 			return key.compareTo(other)
 		}
-		
+
 		override fun toString(): String {
 			return "$key=$value"
 		}
 	}
-	
-	
+
+
 	class EntrySet<K : Comparable<K>, V>(private val map: ArrayMap<K, V>) : Set<Map.Entry<K, V>> {
 		override val size: Int
 			get() = map.size
-		
+
 		override fun contains(element: Map.Entry<K, V>): Boolean {
 			return map[element.key] == element.value
 		}
-		
+
 		override fun containsAll(elements: Collection<Map.Entry<K, V>>): Boolean {
 			elements.forEach {
 				if (!contains(it)) return false
 			}
 			return true
 		}
-		
+
 		override fun isEmpty(): Boolean {
 			return map.isEmpty()
 		}
-		
+
 		override fun iterator(): Iterator<Map.Entry<K, V>> {
 			return MapIterator(map)
 		}
 	}
-	
+
 	class MapIterator<K : Comparable<K>, V>(private val map: ArrayMap<K, V>) : Iterator<Map.Entry<K, V>> {
 		private var index = 0
-		
+
 		override fun hasNext(): Boolean {
 			while (index <= map.end && map.arr[index++] == null);
 			index--
 			return index < map.end
 		}
-		
+
 		override fun next(): Map.Entry<K, V> {
 			return map.arr[index++]!!
 		}
 	}
-	
+
 	class KeySet<K : Comparable<K>>(private val map: ArrayMap<K, *>) : Set<K> {
 		override val size: Int
 			get() = map.size
-		
+
 		override fun contains(element: K): Boolean {
 			return map.containsKey(element)
 		}
-		
+
 		override fun containsAll(elements: Collection<K>): Boolean {
 			elements.forEach {
 				if (!map.containsKey(it)) return false
 			}
 			return true
 		}
-		
+
 		override fun isEmpty(): Boolean {
 			return size == 0
 		}
-		
+
 		override fun iterator(): Iterator<K> {
 			return KeyIterator(map)
 		}
 	}
-	
+
 	class KeyIterator<K : Comparable<K>>(map: ArrayMap<K, *>) : Iterator<K> {
 		private val iterator = map.iterator()
 		override fun hasNext(): Boolean {
 			return iterator.hasNext()
 		}
-		
+
 		override fun next(): K {
 			return iterator.next().key
 		}
 	}
-	
+
 	class ValueCollection<V>(private val map: ArrayMap<*, V>) : Collection<V> {
 		override val size: Int
 			get() = map.size
-		
+
 		override fun contains(element: V): Boolean {
 			return map.containsValue(element)
 		}
-		
+
 		override fun containsAll(elements: Collection<V>): Boolean {
 			elements.forEach {
 				if (!map.containsValue(it)) return false
 			}
 			return true
 		}
-		
+
 		override fun isEmpty(): Boolean {
 			return size == 0
 		}
-		
+
 		override fun iterator(): Iterator<V> {
 			return ValueIterator(map)
 		}
 	}
-	
+
 	class ValueIterator<V>(map: ArrayMap<*, V>) : Iterator<V> {
 		private val iterator = map.iterator()
-		
+
 		override fun hasNext(): Boolean {
 			return iterator.hasNext()
 		}
-		
+
 		override fun next(): V {
 			return iterator.next().value
 		}
