f4a57bf7a33058d03ee20be25be17e9afb27ab60
tursom <tursom@foxmail.com>
2019-08-19 21:26:10 +0800

    添加异步文件的封装

--- src/main/kotlin/cn/tursom/utils/AsyncFile.kt
+++ src/main/kotlin/cn/tursom/utils/AsyncFile.kt
@@ -1,45 +1,76 @@
 package cn.tursom.utils
 
 import java.nio.ByteBuffer
-import java.nio.file.Path
-import java.nio.file.StandardOpenOption
 import java.nio.channels.AsynchronousFileChannel
 import java.nio.channels.CompletionHandler
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.nio.file.StandardOpenOption
 import kotlin.coroutines.Continuation
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
 
 
-class AsyncFile(val path: Path, vararg standardOpenOption: StandardOpenOption) {
-	var fileChannel = AsynchronousFileChannel.open(path, *standardOpenOption)
+@Suppress("MemberVisibilityCanBePrivate")
+class AsyncFile(val path: Path) {
+	constructor(path: String) : this(Paths.get(path))
+
+	var exists = false
+	val writeChannel: AsynchronousFileChannel by lazy { AsynchronousFileChannel.open(path, StandardOpenOption.WRITE) }
+	val readChannel: AsynchronousFileChannel by lazy { AsynchronousFileChannel.open(path, StandardOpenOption.READ) }
 
 	fun write(buffer: ByteBuffer, position: Long = 0) {
-		fileChannel.write(buffer, position)
+		create()
+		writeChannel.write(buffer, position)
 	}
 
 	suspend fun writeAndWait(buffer: ByteBuffer, position: Long = 0): Int {
+		create()
 		return suspendCoroutine {
-			fileChannel.write(buffer, position, it, handler)
+			writeChannel.write(buffer, position, it, handler)
 		}
 	}
 
-	suspend fun read(buffer: ByteBuffer, position: Long = 0): Int {
+	fun append(buffer: ByteBuffer, position: Long = size()) {
+		create()
+		writeChannel.write(buffer, position)
+	}
+
+	suspend fun appendAndWait(buffer: ByteBuffer, position: Long = size()): Int {
+		create()
 		return suspendCoroutine {
-			fileChannel.read(buffer, position, it, handler)
+			writeChannel.write(buffer, position, it, handler)
 		}
 	}
+
+	suspend fun read(buffer: ByteBuffer, position: Long = size()): Int {
+		return suspendCoroutine {
+			readChannel.read(buffer, position, it, handler)
+		}
+	}
+
+	fun create() = if (exists || !Files.exists(path)) {
+		Files.createFile(path)
+		exists = true
+		true
+	} else {
+		false
+	}
+
+	fun delete(): Boolean {
+		exists = false
+		return Files.deleteIfExists(path)
+	}
+
+	fun size() = Files.size(path)
 
 	companion object {
 		@JvmStatic
 		val handler = object : CompletionHandler<Int, Continuation<Int>> {
-			override fun completed(result: Int, attachment: Continuation<Int>) {
-				attachment.resume(result)
-			}
-
-			override fun failed(exc: Throwable, attachment: Continuation<Int>) {
-				attachment.resumeWithException(exc)
-			}
+			override fun completed(result: Int, attachment: Continuation<Int>) = attachment.resume(result)
+			override fun failed(exc: Throwable, attachment: Continuation<Int>) = attachment.resumeWithException(exc)
 		}
 	}
 }
